262978ce06c5e2f9a95c16e9216cc55d62a941dc
veronica <veronicadigiacomo190@gmail.com>
2024-09-24 15:43:41 +0200

    aggiunta implementazione to do list
    Signed-off-by: veronica <veronicadigiacomo190@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { signal, computed } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
-export class AppComponent {
-  title = 'angular-to-do-list';
+export class AppComponent implements OnInit {
+  todos = signal<Todo[]>([]);
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get<Todo[]>(`http://localhost:3000/todos`).subscribe((res) => {
+      this.todos.set(res);
+    });
+  }
+
+  totalCompleted = computed(
+    () => this.todos().filter((t) => t.completed).length
+  );
+  totalTodos = computed(() => this.todos().filter((t) => !t.completed).length);
+
+  removeTodo(todoToRemove: Todo) {
+    this.http
+      .delete(`http://localhost:3000/todos/${todoToRemove.id}`)
+      .subscribe(() => {
+        this.todos.update((todos) =>
+          todos.filter((todo) => todo.id !== todoToRemove.id)
+        );
+      });
+  }
+
+  addTodo(input: HTMLInputElement) {
+    this.http
+      .post<Todo>(`http://localhost:3000/todos`, {
+        title: input.value,
+        completed: false,
+      })
+      .subscribe((newTodo) => {
+        this.todos.update((todos) => [...todos, newTodo]);
+        input.value = '';
+      });
+  }
+
+  toggleTodo(todoToToggle: Todo) {
+    this.http
+      .patch<Todo>(`http://localhost:3000/todos/${todoToToggle.id}`, {
+        ...todoToToggle,
+        completed: !todoToToggle.completed,
+      })
+      .subscribe((res) => {
+        this.todos.update((todos) => {
+          return todos.map((t) => (t.id === todoToToggle.id ? res : t));
+        });
+      });
+  }
+}
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
 }
